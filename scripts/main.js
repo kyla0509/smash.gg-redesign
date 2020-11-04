aaecb9f9c40a7dec7adfb7cae39349507efcfd09
Kyla <57500265+kyla0509@users.noreply.github.com>
2020-11-04 16:33:58 -0500

    Added JS

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+}
+
+/*   if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+*/
