cf5b64e2997e091f158f8926e64ecb58df504884
Kyla <57500265+kyla0509@users.noreply.github.com>
2020-12-17 00:49:54 -0500

    Final Version of Website

---
+++ 2020-12-8/scripts/confirm_settings.js
@@ -0,0 +1,10 @@
+function confirm_settings(node) {
+  return confirm("Settings confirmed!");
+}
+
+/*   if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+*/
